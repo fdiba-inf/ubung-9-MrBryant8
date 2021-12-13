e1704ac9ac196909c2bcfb8bd5ce8e2fe04083dd
MrBryant8 <mincho.tanev@fdiba.tu-sofia.bg>
2021-12-13 18:59:09 +0200

    qweqweqwe

--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -3,8 +3,8 @@ package exercise9;
 public class EllipseDemo {
     public static void main(String[] args) {
         Ellipse ellipse1 = new Ellipse();
-        Point startPoint2 = new Point(1, 1);
-        Ellipse ellipse2 = new Ellipse(startPoint2, 2, 3);
+        Point startPoint2 = new Point(2, 3);
+        Ellipse ellipse2 = new Ellipse(startPoint2, 5, 6);
         Ellipse ellipse3 = new Ellipse(ellipse2);
         Ellipse ellipse4 = new Ellipse();
         ellipse4.initialize();
